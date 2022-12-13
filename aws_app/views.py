cbd707dcdbe9d931e893322917f679f00a702d5d
KimBitrus26 <bitruschoji4real@gmail.com>
2022-12-13 10:35:03 +0100

    new agagagaga

--- aws_app/views.py
+++ aws_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("works now again")
+    return HttpResponse("works again")
