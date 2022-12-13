d4a31aed68c975823f55f73e218ee57153c96f1a
KimBitrus26 <bitruschoji4real@gmail.com>
2022-12-13 11:28:57 +0100

    works

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,7 @@ cd /home/ubuntu/django-app
 
 python3.8 -m venv venv
 source venv/bin/activate
+pip install install -r requirements.txt
 python3.8 manage.py migrate
 
 python3.8 manage.py runserver 0.0.0.0:8000
