d496d6d729783b655fbd1658b1334b6baed9f939
KimBitrus26 <bitruschoji4real@gmail.com>
2022-12-13 14:44:26 +0100

    works timeout

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/ubuntu/django-app
+# cd /home/ubuntu/django-app
 
-python3.8 -m venv venv
-source venv/bin/activate
+# python3.8 -m venv venv
+# source venv/bin/activate
 
 python3.8 manage.py migrate
 
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /home/ubuntu/django-app
+# cd /home/ubuntu/django-app
 
-python3.8 -m venv venv
-source venv/bin/activate
+# python3.8 -m venv venv
+# source venv/bin/activate
 
 python3.8 manage.py runserver 0.0.0.0:8000
